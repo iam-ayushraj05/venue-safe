c4c8aaf70fd25420232c4e93030f0147c28e97f0
iam-ayushraj05 <offayushraj1006@gmail.com>
2026-04-28 15:29:54 +0530

    fix: deploy to cloud run (dockerignore and port binding)

--- server/server.js
+++ server/server.js
@@ -390,6 +390,6 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
